9183eb53dcda19be1fbe00a2d3ab15201d30e0d3
MPL <manbobo2012@gmail.com>
2020-03-11 12:52:18 +0800

    update

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,43 @@
+import boto3
 def lambda_handler(event, context):
-   number = event['value'];
-   count = int (number) + 1;
-   resp = {
+    type = event['type'];
+    id =event['id'];
+    # this will create dynamodb resource object and
+    # here dynamodb is resource name
+    client = boto3.resource('dynamodb')
+    
+    # this will search for dynamoDB table 
+    # your table name may be different
+    table = client.Table("votting_table")
+    
+    if type=="get":
+        db_value=table.get_item(Key={'id': id})['Item']['Count']
+        resp = {
         "statusCode": 200,
         "headers": {
             "Access-Control-Allow-Origin": "*",
         },
         "body": {
-            "Count": count
+            "Count": db_value
         }
-    }
-   return resp
+        }
+    else:
+    
+        db_value=table.get_item(Key={'id': id})['Item']['Count']
+        table.put_item(Item= {'id': id,'Count':  db_value+1})
+        new_value=table.get_item(Key={'id': id})['Item']['Count']
+                    
+        resp = {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+            },
+            "body": {
+                "Count": new_value
+            }
+        }
+    return resp
+
+    
+
+
